6f9b7cd5cc503ea6024fa60976a6c5e464f604d3
Joakim Nohlgård <joakim@nohlgard.se>
2024-10-17 16:37:13 +0200

    bridge_master_spec tidy

--- internal/app/machined/pkg/adapters/network/bridge_master_spec.go
+++ internal/app/machined/pkg/adapters/network/bridge_master_spec.go
@@ -25,39 +25,26 @@ type bridgeMaster struct {
 	*network.BridgeMasterSpec
 }
 
+func bool01(b bool) uint8 {
+	if b {
+		return 1
+	}
+	return 0
+}
+
 // Encode the BridgeMasterSpec into netlink attributes.
 func (a bridgeMaster) Encode() ([]byte, error) {
 	bridge := a.BridgeMasterSpec
 
 	encoder := netlink.NewAttributeEncoder()
 
-	stpEnabled := 0
-	if bridge.STP.Enabled {
-		stpEnabled = 1
-	}
-
-	vlanFiltering := 0
+	encoder.Uint32(unix.IFLA_BR_STP_STATE, uint32(bool01(bridge.STP.Enabled)))
+	encoder.Uint8(unix.IFLA_BR_VLAN_FILTERING, bool01(bridge.VLAN.FilteringEnabled))
+	encoder.Uint8(unix.IFLA_BR_VLAN_STATS_ENABLED, bool01(bridge.VLAN.StatsEnabled))
+	encoder.Uint8(unix.IFLA_BR_VLAN_STATS_PER_PORT, bool01(bridge.VLAN.StatsPerPort))
 	if bridge.VLAN.FilteringEnabled {
-		vlanFiltering = 1
-	}
-
-	vlanStats := 0
-	if bridge.VLAN.StatsEnabled {
-		vlanStats = 1
-	}
-
-	vlanStatsPerPort := 0
-	if bridge.VLAN.StatsPerPort {
-		vlanStatsPerPort = 1
-	}
-
-	encoder.Uint32(unix.IFLA_BR_STP_STATE, uint32(stpEnabled))
-	encoder.Uint8(unix.IFLA_BR_VLAN_FILTERING, uint8(vlanFiltering))
-	if vlanFiltering != 0 {
 		encoder.Uint16(unix.IFLA_BR_VLAN_DEFAULT_PVID, bridge.VLAN.DefaultPVID)
 	}
-	encoder.Uint8(unix.IFLA_BR_VLAN_STATS_ENABLED, uint8(vlanStats))
-	encoder.Uint8(unix.IFLA_BR_VLAN_STATS_PER_PORT, uint8(vlanStatsPerPort))
 
 	return encoder.Encode()
 }
@@ -77,12 +64,12 @@ func (a bridgeMaster) Decode(data []byte) error {
 			bridge.STP.Enabled = decoder.Uint32() == 1
 		case unix.IFLA_BR_VLAN_FILTERING:
 			bridge.VLAN.FilteringEnabled = decoder.Uint8() == 1
-		case unix.IFLA_BR_VLAN_DEFAULT_PVID:
-			bridge.VLAN.DefaultPVID = decoder.Uint16()
 		case unix.IFLA_BR_VLAN_STATS_ENABLED:
 			bridge.VLAN.StatsEnabled = decoder.Uint8() == 1
 		case unix.IFLA_BR_VLAN_STATS_PER_PORT:
 			bridge.VLAN.StatsPerPort = decoder.Uint8() == 1
+		case unix.IFLA_BR_VLAN_DEFAULT_PVID:
+			bridge.VLAN.DefaultPVID = decoder.Uint16()
 		}
 	}
 
